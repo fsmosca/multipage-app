8573c9b1a6c3a066f56a013dff09c9172e2ed4c6
fsmosca <fsmosca@users.noreply.github.com>
2024-04-23 22:44:10 +0800

    Create home.py

---
+++ modules/home.py
@@ -0,0 +1,5 @@
+BALL_SPORTS = '''
+**Ball sports** are athletic activities that involve the use of a **ball** as the primary equipment.
+Participants compete by **hitting, throwing, or kicking** the ball, usually aiming to score points
+by getting the ball into a designated area or goal while following a **set of rules**.
+'''
