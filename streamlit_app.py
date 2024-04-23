19ab9d391b3fb98adefbe08a10ac9ea6c9a928fa
fsmosca <fsmosca@users.noreply.github.com>
2024-04-23 22:44:38 +0800

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from modules.nav import Nav
+from modules.home import BALL_SPORTS
+
+
+st.set_page_config(
+    layout='centered',
+    page_title='Sports'
+)
+
+
+def main():
+    Nav()
+    st.header('🏡 Home')
+
+    st.markdown(BALL_SPORTS)
+
+
+if __name__ == '__main__':
+    main()
